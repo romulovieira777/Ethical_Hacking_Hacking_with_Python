28cba436201cc9d6e62770fd5405a86feaf22df2
Romulo Vieira <romulo.vieira777@gmail.com>
2022-07-19 11:13:58 -0300

    Attack Surface

---
+++ Section_03_Attack_Surface/Code_Python/subdomain.py
@@ -0,0 +1,16 @@
+import requests
+import sys
+
+sub_list = open("../Documents/wordlist.txt").read()
+subdomains = sub_list.splitlines()
+
+for sub in subdomains:
+    sub_domain = f"http://{sub}.{sys.argv[1]}"
+    print(sub_domain)
+
+    try:
+        requests.get(sub_domain)
+    except requests.ConnectionError:
+        pass
+    else:
+        print("Found: ", sub_domain)
